06429f126cc23ddd41056df8cd9df6b76b9bc81c
Riccardo Brioschi <riccardo.brioschiweb@gmail.com>
2022-11-22 22:35:59 +0100

    fixing plot

--- helpers.py
+++ helpers.py
@@ -58,13 +58,14 @@ def visualize_LvsN(losses_test, losses_train):
         losses_train: array of shape (num_epochs,) containing train error
     """
     fig,axs = plt.subplots(1,2, figsize=(15,10))
-    axs[1].plot(range(1,len(losses_test)+1), losses_test, 'ro-', label='Loss(MSE) test')
-    axs[1].set(title='MSE w.r.t. number of epochs',xlabel='epochs',ylabel='test_loss(MSE)')
-    plt.grid()
     axs[0].plot(range(1,len(losses_train)+1), losses_train, 'bo-', label='Loss(MSE) train')
     axs[0].set(title='MSE w.r.t. number of epochs',xlabel='epochs',ylabel='train_loss(MSE)')
-    plt.grid()
-    plt.legend()
+    axs[0].grid(visible=True)
+    axs[0].legend()
+    axs[1].plot(range(1,len(losses_test)+1), losses_test, 'ro-', label='Loss(MSE) test')
+    axs[1].set(title='MSE w.r.t. number of epochs',xlabel='epochs',ylabel='test_loss(MSE)')
+    axs[1].grid(visible=True)
+    axs[1].legend()
 
 def correlation_plot(predicted, y):
     fig, ax = plt.subplots(figsize=(12, 8))
