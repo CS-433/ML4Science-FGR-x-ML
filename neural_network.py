2beebcd5feaf924c6f12fbce4e67a2dc3053fb8a
fededg <digennarof.00@gmail.com>
2022-12-03 18:45:44 +0100

    new architectures

--- neural_network.py
+++ neural_network.py
@@ -15,13 +15,14 @@ class my_FNN_increasing(nn.Module):
         super().__init__()
         # Generating and initializing each layer
         self.l1 = nn.Linear(num_feature,64,dtype=dtype)
-        self.reLU1 = nn.ReLU()
+        self.reLU1 = nn.LeakyReLU()
         self.l2 = nn.Linear(64,128,dtype=dtype)
-        self.reLU2 = nn.ReLU()
+        self.reLU2 = nn.LeakyReLU()
         self.l3 = nn.Linear(128,256,dtype=dtype)
-        self.reLU3 = nn.ReLU()
+        self.reLU3 = nn.LeakyReLU()
         self.l4 = nn.Linear(256,1,dtype=dtype)
         #self.l5 = nn.Sigmoid()
+        self.dropout = nn.Dropout(0.25)
 
     def forward(self,input):
 
@@ -29,10 +30,13 @@ class my_FNN_increasing(nn.Module):
 
         out = self.l1(input)
         out = self.reLU1(out)
+        out = self.dropout(out)
         out = self.l2(out)
         out = self.reLU2(out)
+        out = self.dropout(out)
         out = self.l3(out)
         out = self.reLU3(out)
+        out = self.dropout(out)
         out = self.l4(out)
         #out = self.l5(out)
         
@@ -49,17 +53,28 @@ class my_FNN_mirror(nn.Module):
         super().__init__()
         # Generating and initializing each layer
         self.l1 = nn.Linear(num_feature,64,dtype=dtype)
+        self.reLU1 = nn.LeakyReLU()
         self.l2 = nn.Linear(64,128,dtype=dtype)
+        self.reLU2 = nn.LeakyReLU()
         self.l3 = nn.Linear(128,64,dtype=dtype)
+        self.reLU3 = nn.LeakyReLU()
         self.l4 = nn.Linear(64,1,dtype=dtype)
+
+        self.dropout = nn.Dropout(0.25)
 
     def forward(self,input):
 
         "Function to implement the forward pass"
 
         out = self.l1(input)
+        out = self.reLU1(out)
+        out = self.dropout(out)
         out = self.l2(out)
+        out = self.reLU2(out)
+        out = self.dropout(out)
         out = self.l3(out)
+        out = self.reLU3(out)
+        out = self.dropout(out)
         out = self.l4(out)
 
         return out
