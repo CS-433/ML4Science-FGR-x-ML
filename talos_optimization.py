4b35e6ca6f5e2f31076654fe1d3d67387f0547b0
Riccardo Brioschi <riccardo.brioschiweb@gmail.com>
2022-12-08 21:09:00 +0100

    fixing talos

--- talos_optimization.py
+++ talos_optimization.py
@@ -17,24 +17,7 @@ import talos
 from talos.utils import lr_normalizer
 
 
-
 ##### GLOBAL ENVIRONMENT #####
-
-# Defining useful class to convert data in a format accepted by Pytorch
-
-class Customized_dataset(Dataset):
-
-    def __init__(self,X,target):
-        super().__init__()
-        self.X = torch.tensor(X)
-        self.target = torch.tensor(target)
-    
-    def __len__(self):
-        return self.X.shape[0]
-
-    def __getitem__(self, idx):
-
-        return self.X[idx,:], self.target[idx]
 
 # Defining parameters to test during optimization
 
@@ -54,82 +37,121 @@ class customized_increasing_NN(nn.Module, talos.utils.TorchHistory):
         self.dropout = nn.Dropout(p['dropout'])
         self.activation = p['activation']
         self.starting_linear = nn.Linear(num_features, p['hidden_layer_size'], dtype=dtype)
+        self.__nr_layers = p['nr_layers']
+        self.__hidden_layer_size = p['hidden_layer_size']
+        self.ending_layer = nn.Linear((self.__nr_layers)*self.__hidden_layer_size, 1, dtype=dtype)
     
     def forward(self,input):
 
         out = self.dropout(self.activation(self.starting_linear(input)))
-
-        for idx_layer in range(1,p['nr_layers']):
-            self.linear = nn.Linear(p['hidden:layer_size']*(idx_layer), p['hidden:layer_size']*(idx_layer+1))
+        for idx_layer in range(1,self.__nr_layers):
+            self.linear = nn.Linear(self.__hidden_layer_size*(idx_layer), self.__hidden_layer_size*(idx_layer+1), dtype = params.dtype).cuda()
             out = self.dropout(self.activation(self.linear(out)))
+        out = self.ending_layer(out)
         return out
 
 # Defining function to test neural network with the current set of parameters
 
 def optimization_using_talos(X_train, y_train, X_test, y_test, p):
 
+    # To better train the model, instead of using the whole train data at the same time, we divide it in batches using a fixed batch size
+    # Converting data into pytorch dataset object
+    gc.collect()
+
+    train_dataset = Customized_dataset(X_train,y_train)
+    test_dataset = Customized_dataset(X_test,y_test)
+
+    # Divide train and test data into iterable batches
+    train_loader = DataLoader(dataset=train_dataset,batch_size=params.batch_size, shuffle=True,
+                                num_workers=2, pin_memory=torch.cuda.is_available())
+
+    test_loader = DataLoader(dataset=test_dataset,batch_size=params.batch_size, shuffle=True,
+                                pin_memory=torch.cuda.is_available())
+    
+    del train_dataset
+    del test_dataset
+
+    gc.collect()
+
     # Initializing the model
     model = customized_increasing_NN(p,X_train.shape[1],params.dtype)
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    # Initializing tools for learning process
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr = p['lr'])
-    scheduler = ReduceLROnPlateau(optimizer = optimizer, mode = 'min', factor = 0.1, patience = 20, min_lr=1e-12, verbose=True)
+    scheduler = ReduceLROnPlateau(optimizer = optimizer, mode = 'min', factor = 0.1, patience = 20, min_lr=1e-12, verbose=False)
+    
     # Initializing history of the net
     model.init_history()
 
-    # INitializing list to store train loss
-
     # Training the model
+    model.train()
+
     for epoch in range(params.epochs):
 
-        # Training and updating weights
-        optimizer.zero_grad()
-        model.train()
-        output = model(X_train)
-        loss = criterion(output, y_train)
-        loss.backward()
-        optimizer.step()
-        scheduler.step(loss.item())
-        # Storing the current loss value using talos history
-        model.append_loss(loss.item())
+        # Defining support data structure
+        loss_train, loss_test = [], []
 
-        # Using validation data
-        with torch.no_grad():
-            output = model(X_test)
-            loss = criterion(output,y_test)
-            model.append_val_loss(loss.item())
+        for (data,target) in train_loader:
+
+            # Moving test data to GPU if possible
+            if torch.cuda.is_available():
+                data, target = data.cuda(), target.cuda()
+
+            # Training and updating weights
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            scheduler.step(loss.item())
+            # Storing current loss value in a provisional list
+            loss_train.append(loss.item())
+        
+        # Storing the current loss value using talos history
+        model.append_loss(np.mean(loss_train))
+
+        # Using test data
+        for (data,target) in test_loader:
+
+            # Moving test data to GPU if possible
+            if torch.cuda.is_available():
+                data, target = data.cuda(), target.cuda()
+
+            with torch.no_grad():
+                output = model(data)
+                loss = criterion(output, target)
+                # Storing current loss value in a provisional list
+                loss_test.append(loss.item())
+                
+        # Storing the current loss value using talos history
+        model.append_val_loss(np.mean(loss_test))
 
     return model, model.parameters()
 
 ##### MAIN SCRIPT TO RUN #####
 
 if __name__ == '__main__':
-
-    gc.collect()
     
     # Loading dataset
-    
-    #X, y, dim_feat = get_single_dataset(params.path_file)
-    X, y, dim_feat, mean_halo, std_halo = get_dataset_LH_fixed('./outputs_test2/LH_0', 
-                                        features = ['MassHalo','Nsubs','MassBH','dotMassBH','SFR','Flux','Density','Temp','VelHalo','z','M_HI'],
-                                        masking = True)
+    # The function to load data depends on the redshift(s) and simulation(s) one is considering
+    X, y, dim_feat, mean_halo, std_halo = get_dataset_LH_fixed('./outputs_test2/LH_0')
 
-    # Scaling the output
+    # Scaling the output. By computing the logarithmic transformation, we want that our network learns the order of the mass and as many digits as possible regarding its magnitude
     y = np.log10(y)
 
-    # Splitting data into train and test set
-    # 75 % train, 20% test, 5% validation
-    X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.25, random_state=2022)
+    # Splitting data into train and test set: 75 % train, 25%
+    X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.25, random_state=2022) # we fix the random state for reproducibility purpose
 
-    X_test,X_val,y_test,y_val = train_test_split(X_test, y_test, test_size=0.2, random_state=2022)
-
-    # Converting data into pytorch dataset object
-    train_dataset = Customized_dataset(X_train,y_train)
-
-    test_dataset = Customized_dataset(X_test,y_test)
-
+    # COmputing hyperparameters tuning using talos library
 
     scan_object = talos.Scan(x=X_train, y=y_train, x_val=X_test, y_val=y_test, params=p, model=optimization_using_talos,
-        experiment_name='Optimization_of_network', round_limit=100)
+        experiment_name='Optimization_of_network', fraction_limit = 0.01)
+
+    scan_object.data.to_csv('./results.csv')
+
 
 
 
