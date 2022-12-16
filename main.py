fixing log transofmr + defining final architecture for masking

main.py
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     # The function to load data depends on the redshift(s) and simulation(s) one is considering -> here z = 0.95,
     X, y, dim_feat = get_single_dataset('./outputs_test2/LH_0/MHI_LH0_z=0.950.hdf5', masking=True)
 
-    # Scaling the output. By computing the logarithmic transformation, we want that our network learns the order of the mass and as many digits as possible regarding its magnitude
-    y = np.log10(y)
+    # Scaling the output. By computing the logarithmic transformation, we want that our network learns the order of the mass and as many digits as possible regarding its magnitude. Notice that we add 1 to the target in order to avoid problems with small values
+    y = np.log10(1 + y)
 
     # Splitting data into train and test set: 75 % train, 20% test, 5% validation
     X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.25, random_state=42) # we fix the random state for reproducibility purpose

neural_network.py
@@ -41,7 +41,9 @@ class customized_increasing_NN(nn.Module, talos.utils.TorchHistory):
 class my_FNN_increasing_masking(nn.Module):
 
     """Class to define the architecture. The hyperparameters chosen after talos optimization are:
-    {'nr_hidden_layer:4, hidden_layers_size:16, dropout_rate:0.1, lr:0.1} -> for masking
+    {'nr_layers:4, hidden_layer_size:16, activation: ReLU(), dropout_rate:0.05, lr:0.01}
+    This architecture has been used on the whole dataset (every simulation) after applying the masking procedure.
+    You need to set lr = 0.01 in params before using this architecture.
     """
 
     def __init__(self,num_feature, dtype):
@@ -57,7 +59,7 @@ class my_FNN_increasing_masking(nn.Module):
         self.l4 = nn.Linear(64,128,dtype=dtype) 
         self.reLU4 = nn.ReLU()
         self.l5 = nn.Linear(128,1,dtype=dtype) 
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(0.05)
 
     def forward(self,input):
 

talos_optimization.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     X, y, dim_feat= get_dataset_LH_fixed('./outputs_test2/LH_0')
 
     # Scaling the output. By computing the logarithmic transformation, we want that our network learns the order of the mass and as many digits as possible regarding its magnitude
-    y = np.log10(y)
+    y = np.log10(1 + y)
 
     # Splitting data into train and test set: 75 % train, 25%
     X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.25, random_state=42) # we fix the random state for reproducibility purpose